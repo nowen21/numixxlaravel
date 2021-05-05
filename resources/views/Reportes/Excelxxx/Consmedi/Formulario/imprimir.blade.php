cbcb18a0eed2783210121322a031f33b54a12abf
Ing. José Dúmar Jiménez Ruíz <nowen21@gmail.com>
2021-05-05 07:23:56 -0500

    se agrega el npt al reporte de medicamentos consumidos

--- resources/views/Reportes/Excelxxx/Consmedi/Formulario/imprimir.blade.php
+++ resources/views/Reportes/Excelxxx/Consmedi/Formulario/imprimir.blade.php
@@ -4,10 +4,12 @@
             <th scope="col">FECHA DEL MEDICAMENTO CONSUMIDO</th>
             <th scope="col">NOMBRE DE LA CL&Iacute;NICA</th>
             <th scope="col">NOMBRE DEL PACIENTE</th>
+            <th scope="col">NPT</th>
             <th scope="col">C&Oacute;DIGO DEL MEDICAMENTO</th>
             <th scope="col">NOMBRE DEL MEDICAMENTO</th>
             <th scope="col">UNIDAD DE MEDIDA</th>
             <th scope="col">CANTIDAD DE ML CONSUMIDOS</th>
+
         </tr>
     </thead>
     <tbody>
@@ -20,6 +22,7 @@
             <th>{{$registro->created_at}}</th>
             <td>{{$formulax->sis_clinica->clinica->clinica}}</td>
             <td>{{ $paciente->nombres}} {{ $paciente->apellidos}}</td>
+            <td>{{ $paciente->npt->nombre}} </td>
             <td>{{$registro->dformula->medicame_id}}</td>
             <td>{{$registro->dformula->medicame->nombgene}}</td>
             <td>{{$registro->dformula->medicame->casa->unidmedi}}</td>
